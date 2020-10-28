a2bb7abee9297a6b4f69463940501be2afb89618
Matic Leva <vertical3life@gmail.com>
2020-10-28 19:34:13 +0100

    Add grid and tiles

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -7,20 +10,33 @@ namespace piton
     public class Game1 : Game
     {
         private Texture2D _spritesheet;
-
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private const int TileSize = 32;
+        private const int GridSize = 30;
+
+        private List<Tile> _grid;
+        private readonly Grass _grass = new Grass();
+
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            _grid = Enumerable.Repeat<Tile>(null, GridSize * GridSize).ToList();
+            _grid[GridSize] = new SnakeTail();
+            _grid[GridSize + 1] = new SnakeBody();
+            _grid[GridSize + 2] = new SnakeHead();
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferHeight = TileSize * GridSize;
+            _graphics.PreferredBackBufferWidth = TileSize * GridSize;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -49,11 +65,24 @@ namespace piton
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
-            _spriteBatch.Draw(_spritesheet, new Vector2(0, 0), new Rectangle(384 * 2, 0, 384, 384), Color.White, 0,
-                Vector2.Zero, Vector2.One * 0.5f, SpriteEffects.None, 0);
+            for (int i = 0; i < _grid.Count; i++)
+            {
+                DrawTile(new Vector2(i % GridSize, (int) Math.Floor((double) i / GridSize)), _grass.sprite);
+                if (_grid[i] != null)
+                    DrawTile(new Vector2(i % GridSize, (int) Math.Floor((double) i / GridSize)), _grid[i].sprite);
+            }
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        private void DrawTile(Vector2 position, Vector2 source)
+        {
+            _spriteBatch.Draw(_spritesheet, position * TileSize,
+                new Rectangle(384 * (int) source.X, 385 * (int) source.Y, 384, 384),
+                Color.White, 0, Vector2.Zero, Vector2.One / 384f * TileSize,
+                SpriteEffects.None, 0);
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
 # Piton
 
-- 2D, grid/mreža
+- 2D, grid/mreža ✔
+    - en tile je velik 32x32 ✔
+- kača se premika naprej
+- kača se obrne, če pritisnemo pravo tipko
+    - kača se ne more obrnit nazaj
+    - input batching
 - kača začne s tremi koščki, vsakič ko poje enega, se podaljša
 - ko se hrana poje, moremo ustvarit novo na random poziciji
 - če se zaleti v samo sebe ali rob mreže, je game over
